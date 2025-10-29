9d8d446e8bf6dc14cbf7e31985663c63909b8aba
Felipe63010 <felipecortesnascimento@gmail.com>
2025-10-29 20:30:56 -0300

    Campo_Minado
    Final

---
+++ Campo_Minado.py
@@ -0,0 +1,166 @@
+import random
+import copy 
+
+while True:
+    try:
+        linhas = int(input("Digite a altura do mapa (máximo 10):"))
+        colunas = int(input("Digite a largura do mapa (máximo 10):"))
+        if linhas <= 10 and colunas <= 10 and linhas > 0 and colunas > 0:
+            mapa = []
+            numero_de_minas = int(input("Digite o número minas que deseja enfrentar:"))
+            if numero_de_minas <= (linhas*colunas) and numero_de_minas > 0:
+                for i in range(linhas):
+                    linha = []
+                    for j in range(colunas):
+                        coluna = []
+                        linha.append("*")
+                    mapa.append(linha)
+    
+                for linha in mapa:
+                    print(*linha, sep=" ") #asterisco desempacota lista e sep o que vai saparar
+                break 
+            else:
+                print("Inválido, o número de minas não pode ser 0 e nem superior ao tamanho do mapa! ")
+        else:
+            print("Valor inválido!")        
+    except(ValueError):
+        print("Digite valores válidos!")
+
+numero_de_linhas = len(mapa)
+numero_de_colunas = len(mapa[0])
+mapa_invisivel = copy.deepcopy(mapa)
+while numero_de_minas>0:
+    bomba_linha = random.randint(0, numero_de_linhas-1)  
+    bomba_coluna = random.randint(0, numero_de_colunas-1)
+    
+    if not mapa_invisivel[bomba_linha][bomba_coluna]=="m": 
+        mapa_invisivel[bomba_linha][bomba_coluna] = "m"
+        numero_de_minas = numero_de_minas - 1
+    
+print()
+
+def marcar_bomba(mapa):
+    try:
+        marcar_linha = int(input("Digite a linha que você acredita que há mina está: "))
+        marcar_coluna = int(input("Digite a coluna que você acredita que há mina está: "))
+
+        marcar_linha = marcar_linha - 1
+        marcar_coluna = marcar_coluna - 1
+
+        if marcar_linha >= linhas or marcar_coluna >= colunas or marcar_linha < 0 or marcar_coluna < 0:
+            print("Posição escolhida inválida!")
+        else:
+            mapa[marcar_linha][marcar_coluna] = "M"
+
+
+        for linha in mapa:
+            print(*linha, sep=" ")
+    except(ValueError):
+        print("Digite valores válidos!")
+
+def abrir_posicao(mapa):
+
+    try:
+        marcacao_lin = int(input("Digite a linha da posição que deseja abrir: "))
+        marcacao_col = int(input("Digite a coluna da posição que deseja abrir: "))
+
+        marcacao_lin = marcacao_lin - 1
+        marcacao_col = marcacao_col - 1
+
+
+        if marcacao_lin >= linhas or marcacao_col >= colunas or marcacao_lin < 0 or marcacao_col < 0:
+            print("Posição escolhida inválida")
+        else:
+            if mapa_invisivel[marcacao_lin][marcacao_col] == "m":
+                print("Boooom")
+                print("Vocẽ perdeu!")
+                return False
+            else:
+                posicoes_verificadas = set()
+                bomba = 0
+
+                if marcacao_lin + 1 < len(mapa_invisivel):
+                    if mapa_invisivel[marcacao_lin + 1][marcacao_col] == "m" and (marcacao_lin + 1, marcacao_col) not in posicoes_verificadas:
+                        bomba = bomba + 1
+                        posicoes_verificadas.add((marcacao_lin + 1, marcacao_col))
+
+                if marcacao_col + 1 < len(mapa_invisivel[0]):        
+                    if mapa_invisivel[marcacao_lin][marcacao_col + 1] == "m" and (marcacao_lin, marcacao_col + 1) not in posicoes_verificadas:
+                        bomba = bomba + 1
+                        posicoes_verificadas.add((marcacao_lin, marcacao_col + 1))
+
+                if marcacao_lin + 1 < len(mapa_invisivel) and marcacao_col + 1 < len(mapa_invisivel[0]):        
+                    if mapa_invisivel[marcacao_lin + 1][marcacao_col + 1] == "m" and (marcacao_lin + 1, marcacao_col + 1) not in posicoes_verificadas:
+                        bomba = bomba + 1
+                        posicoes_verificadas.add((marcacao_lin + 1, marcacao_col + 1))
+
+                if marcacao_lin - 1 >= 0:
+                    if mapa_invisivel[marcacao_lin - 1][marcacao_col] == "m" and (marcacao_lin - 1, marcacao_col) not in posicoes_verificadas:
+                        bomba = bomba + 1
+                        posicoes_verificadas.add((marcacao_lin - 1, marcacao_col))
+
+                if marcacao_col - 1 >= 0:
+                    if mapa_invisivel[marcacao_lin][marcacao_col - 1] == "m" and (marcacao_lin, marcacao_col - 1) not in posicoes_verificadas:
+                        bomba = bomba + 1
+                        posicoes_verificadas.add((marcacao_lin, marcacao_col - 1))
+
+                if marcacao_lin - 1 >= 0 and marcacao_col - 1 >= 0:
+                    if mapa_invisivel[marcacao_lin - 1][marcacao_col - 1] == "m" and (marcacao_lin - 1, marcacao_col - 1) not in posicoes_verificadas:    
+                        bomba = bomba + 1
+                        posicoes_verificadas.add((marcacao_lin - 1, marcacao_col - 1))
+
+                if marcacao_lin - 1 >= 0 and marcacao_col + 1 < len(mapa_invisivel[0]):
+                    if mapa_invisivel[marcacao_lin - 1][marcacao_col + 1] == "m" and (marcacao_lin - 1, marcacao_col + 1) not in posicoes_verificadas:
+                        bomba = bomba + 1
+                        posicoes_verificadas.add((marcacao_lin - 1, marcacao_col + 1))
+
+                if marcacao_lin + 1 < len(mapa_invisivel) and marcacao_col - 1 >= 0:
+                    if mapa_invisivel[marcacao_lin + 1][marcacao_col - 1] == "m" and (marcacao_lin + 1, marcacao_col - 1) not in posicoes_verificadas:        
+                        bomba = bomba + 1
+                        posicoes_verificadas.add((marcacao_lin + 1, marcacao_col - 1))
+
+                mapa[marcacao_lin][marcacao_col] = (bomba)
+                for linha in mapa:
+                    print(*linha, sep=" ")
+                return True
+    except(ValueError):
+        print("Digite valores válidos!")
+
+
+def verificador_vitoria(mapa, mapa_invisivel):
+    for linha in range(len(mapa_invisivel)):
+        for coluna in range(len(mapa_invisivel[0])):
+            if mapa_invisivel[linha][coluna] == "m":
+                if mapa[linha][coluna] != "M":
+                    return False
+            else:
+                if mapa[linha][coluna] == "M":
+                    return False
+    return True        
+
+    
+while True:
+        try:
+            opcao = int(input("Digite 1 para marcar uma bomba! \n" \
+                              "Digite 2 para abrir uma posição! \n" \
+                              "Digite 3 para desistir! \n"))
+
+            match opcao:
+                case 1:
+                    marcar_bomba(mapa)
+                    if verificador_vitoria(mapa,mapa_invisivel):
+                        print("Parabéns você venceu!")
+                        break
+                    else:
+                        continue
+                case 2: 
+                    verificador_derrota = abrir_posicao(mapa)
+                    if verificador_derrota == False:
+                        break
+                case 3:
+                    print("Desistiu!")
+                    break
+                case _ :
+                    print("Digite um valor do menu!")
+        except(ValueError):
+            print("Erro digite um número do menu")
--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# Campo_minado
- 
+# Campo Minado (Python)
+
+Esse é um código simples que simula o jogo Campo Minado, o qual é jogado no terminal. O jogador escolhe o tamanho do mapa e o número de minas que ele irá enfrentar, o player deve encontrar e marcar todas as minas sem abrir nehuma delas. 
+
+## Funcionalidades
+
+* O tamanho do mapa é definido pelo usuário:
+    * mínimo: 1 linha × 1 coluna
+    * máximo: 10 linhas × 10 colunas
+* O usuário pode decidir a dificuldade do jogo, uma vez que ele que escolhe o número de minas, mas as posições delas são aleatórias.
+* O usuário pode marcar posições com a letra M. 
+* O usuário pode abrir uma posição, assim, tornando possível descobrir quantas bombas existem nas 8 posições em volta da qual ele abriu (na posição aberta fica o número de bombas que se encontra nos arredores).
+
+
+## Formas de Vencer e Perder
+
+* O jogador deve marcar todas as posições que contém mina com a letra M.
+* O jogador não deve abrir uma posição que contém mina, uma vez que caso isso aconteça ele irá perder.
+
+## Como Executar
+* Para jogar, você precisa ter o Python 3 instalado em sua máquina, aí é necessario baixar o arquivo Campo_Minado.py,abrir o terminal e navegar ate o diretorio do arquivo e tem que executar o script com /bin/python3 /home/felipe/Documentos/Campo_Minado/Campo_Minado.py
+
+### Como jogar
+
+* Quando o programa for executado solicitará:
+    * O número de linhas do Mapa
+    * O número de colunas do Mapa
+    * O número de minas do Mapa 
+
+* Em cada rodada, o usuário pode escolher duas opções:
+    * Marcar posições que o jogador acredita que tem bomba com a letra M.
+    * Abrir posição para revelar as 8 posiçoes que fazem fronteira com a cordenada escolhida.
+
+## Interface
+* No início do código os inputs do tamanho do mapa e do número de minas serão mostrados ao usuário com algumas restrições explícitas para que o usuário não cometa erros e caso cometa o usuário volta do início, sendo necessário colocar os inputs novamente.
+* Após essa inserção inicial o mapa será mostrado.
+* Seguindo o código aparecerá um menu, no qual o usuário poderá escolher entre abrir uma posição, marcar uma posição e desistir (nas duas primeiras uma cordenada é pedida).
+* Ao final do jogo se o jogador venceu (marcou todas as minas de forma correta) aparecerá uma mensagem indicando que ele venceu, caso o usuário seja derrotado (tente abrir uma posição, na qual há bomba) uma mensagem indicando que ele perdeu aparecerá e caso ele desista uma mensagem de desistência será exibida. Em todas essas ocasiões o programa é encerrado.
+
+## Descrição Final
+
+* Este projeto utiliza apenas bibliotecas padrão do Python, sem necessidade de instalação de pacotes externos.
+
+*  Random (para a geração aleatória das minas)
+* Copy (para copiar uma matriz em outra)
